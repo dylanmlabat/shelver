2e114b8177024a6415665474ab3c0ef4f67fa58b
Dylan Michael Labat <dml252@live.com>
2019-11-04 22:24:08 -0600

    Add index method to books controller and add seed data to test API route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+books = Book.create(
+  [
+    {
+      author: "Joseph Heller",
+      title: "Catch-22",
+      isbn: 9780684833392,
+      page_count: 453
+    },
+    {
+      author: "Kurt Vonnegut",
+      title: "Mother Night",
+      isbn: 9780385334143,
+      page_count: 282
+    }
+  ]
+)
