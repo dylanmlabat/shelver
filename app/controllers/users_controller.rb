4db4f156f5e68a35025f6b0fa518f0edbcc0ef16
Dylan Michael Labat <dml252@live.com>
2019-11-05 17:45:02 -0600

    Add create method and user_params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:firstname, :lastname, :username, :password_digest)
+    end
+
 end
