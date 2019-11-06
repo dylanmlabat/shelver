169caa8462dc9da6a22db9c1dd4dcd9d3b90f084
Dylan Michael Labat <dml252@live.com>
2019-11-06 12:11:19 -0600

    Add index and show methods to author and user controllers

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -1,2 +1,13 @@
 class API::V1::AuthorsController < ApplicationController
+
+  def index
+    @author = Author.all
+    render json: @authors
+  end
+
+  def show
+    @authors = Author.find(params[:id])
+    render json: @authors
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,15 @@
 class API::V1::UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
