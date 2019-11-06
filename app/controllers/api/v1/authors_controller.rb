4776ce94b9bd3d2d15108c65c0e626a5c1498801
Dylan Michael Labat <dml252@live.com>
2019-11-06 12:15:07 -0600

    Fix typos in author model and controller

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -1,13 +1,13 @@
 class API::V1::AuthorsController < ApplicationController
 
   def index
-    @author = Author.all
+    @authors = Author.all
     render json: @authors
   end
 
   def show
-    @authors = Author.find(params[:id])
-    render json: @authors
+    @author = Author.find(params[:id])
+    render json: @author
   end
 
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ApplicationRecord
   has_many :books
-  has_many :users, :though => :books
+  has_many :users, :through => :books
   has_many :purchases, :through => :users
 end
