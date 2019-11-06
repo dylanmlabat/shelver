50c07570ae25416eebb798e882ecdfbf3cb0fd70
Dylan Michael Labat <dml252@live.com>
2019-11-06 11:24:16 -0600

    Add associations, move author routes to api namespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 
   has_many :purchases
   has_many :books, :through => :purchases
+  has_many :authors, :through => :books
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
+    resources :authors
     resources :books
     resources :users
   end
