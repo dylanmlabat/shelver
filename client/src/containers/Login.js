d167271fdea1fc12059b5b58a58c1d1fb37e3956
Dylan Michael Labat <dml252@live.com>
2019-11-06 16:58:00 -0600

    Create login form, handleChange function, render on App home page

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { login } from '../.././actions/userActions';
+import { login } from '.././actions/userActions';
 
 class Login extends Component {
 
@@ -12,4 +12,34 @@ class Login extends Component {
     }
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return(
+      <form>
+        <input
+          value={this.state.username}
+          name="username"
+          type="text"
+          placeholder="Username"
+          onChange={this.handleChange}>
+        </input>
+        <input
+          value={this.state.password}
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={this.handleChange}>
+        </input>
+        <input value="Log In" type="submit"/>
+      </form>
+    )
+  }
+
 }
+
+export default Login;
