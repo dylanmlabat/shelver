8f30832eb54bfc59103b42ab749954734d0ec0be
Dylan Michael Labat <dml252@live.com>
2019-11-06 17:52:23 -0600

    Fix login route, add create sessions method and helpers

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,11 @@
 class API::V1::SessionsController < ApplicationController
 
   def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    end
   end
 
 end
