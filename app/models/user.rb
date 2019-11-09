525fbe51e892b256080794457aba908c5059bca1
Dylan Michael Labat <dml252@live.com>
2019-11-09 10:52:16 -0600

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
   has_secure_password
 
   has_many :purchases
