be47bebe12acfe2ec8497720a1a3f8f3554c621d
Dylan Michael Labat <dml252@live.com>
2019-11-14 16:51:40 -0600

    Add create book method and params

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -10,4 +10,21 @@ class API::V1::BooksController < ApplicationController
     render json: @book
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book
+    else
+      render json: {
+        error: @book.errors.full_messages.to_sentence
+      }
+    end
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:api_id, :title, :authors, :cover, :publisher, :publish_date, :page_count, :summary)
+    end
+
 end
